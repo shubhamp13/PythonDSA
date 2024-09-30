c1705ff62abfab65c42afe2fe9e6d817f5f20f4a
shubhamp13 <shubhampuri8297@gmail.com>
2024-09-30 22:44:01 +0530

    second

---
+++ arrays/01_first.py
@@ -0,0 +1,6 @@
+import array as arr
+num=arr.array('i',[1,8,3,1,9,9,8])
+print(num)
+#To Print Array Elements With Loop
+for x in num:
+    print(x)
---
+++ arrays/02_sliceing.py
@@ -0,0 +1,15 @@
+import  array as p
+nums=p.array('i',[1,8,0,3,1,9,9,8])
+
+#To Print all elements of an array using slicing operator
+print(nums[:])
+
+#Print Array Elements from 2 to 5
+print(nums[1:5])
+
+#Reverse the array
+print(nums[::-1])
+
+#print elements [1,9,9]
+print(nums[-4:-1])
+
